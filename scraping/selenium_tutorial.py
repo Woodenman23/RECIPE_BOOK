f7a09c05fa2e8517a0e7d0af0e7a69976482a94b
Woodenman23 <josephrfoster@protonmail.com>
2023-11-08 22:29:20 +0000

    twin flames

--- scraping/selenium_tutorial.py
+++ scraping/selenium_tutorial.py
@@ -4,7 +4,24 @@ import os
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
-os.environ['PATH'] += r"C:/Users/fosterj/pyprojects/seleniumdrivers"
+os.environ['PATH'] += r"C:/Users/ASUS/Documents/pyprojects/seleniumdrivers"
+
+url = "https://twinflamesuniverse.com/"
+
+website = url.replace("https://www.", "")
+website = website.replace(".com/", "")
 
 driver = webdriver.Firefox()
+
+driver.get(url)
+
+page_source = driver.page_source
+
+phrase = "here"
+
+count = page_source.lower().count(phrase)
+
+print(f"The phrase {phrase} appears {count} times on my {website} homepage.")
+
+
 

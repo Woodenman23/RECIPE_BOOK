12d8c0c8ed889c0b46148b94d914305398daa78f
Woodenman23 <josephrfoster@protonmail.com>
2023-11-23 19:52:51 +0000

    added exception for if no banner appears on homepage

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from pathlib import Path
 
 from scraping.my_selenium import DRIVER
@@ -16,28 +17,29 @@ def main():
     DRIVER.get(url)
 
     try:
-        # Locate the close button element
-        close_button = WebDriverWait(DRIVER, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".banner__close"))
-    )
+        try:
+            # Locate the close button element
+            close_button = WebDriverWait(DRIVER, 5).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, ".banner__close"))
+        )
+            
+            close_button.click()
+        except TimeoutException:
+            pass
+            search_bar = DRIVER.find_element(By.XPATH, '//*[@id="searchInput"]')
+        
+            
+            search_bar.send_keys(search_term)
+            search_button = DRIVER.find_element(By.XPATH,"/html/body/div[3]/form/fieldset/button")
+            search_button.click()
+            
+            
+            paragraph1_text = _get_text("/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[3]")
+            _add_to_file(paragraph1_text)
+            
 
-        # Click the close button to dismiss the banner
-        close_button.click()
-        
-        search_bar = DRIVER.find_element(By.XPATH, '//*[@id="searchInput"]')
-    
-        
-        search_bar.send_keys(search_term)
-        search_button = DRIVER.find_element(By.XPATH,"/html/body/div[3]/form/fieldset/button")
-        search_button.click()
-        
-         
-        paragraph1_text = _get_text("/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[3]")
-        _add_to_file(paragraph1_text)
-        
 
-
-        time.sleep(5)
+            time.sleep(5)
 
     except:
         print("something failed")
